205eada168ef6feaebb059a9431d9ee582520e49
Mathias Petersen <mathias.petersen95@gmail.com>
2020-09-22 13:57:13 +0200

    Solve 3.2

--- main.cpp
+++ main.cpp
@@ -1,99 +1,52 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sstream>
+#include <iterator>
+#include <algorithm>
+#include <cmath>
+#include <map>
 
-
-typedef enum {
-    wood, stone
-} material;
-typedef struct {
-    int x, y;
-    bool isWall;
-    material type;
-} field;
-
-#define n 12
-#define m 16
-
-
-field playground[n][m];
-
-int x = 5, y = 5;
-
-void print_level() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-
-            if (playground[i][j].x == x && playground[i][j].y == y) {
-                std::cout<<"O";
-            }
-            else if (playground[i][j].isWall) {
-                std::cout<<"*";
-            }
-            else {
-                std::cout<<" ";
-            }
-        }
-        std::cout<<std::endl;
-    }
-}
 
 int main() {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            playground[i][j].x = i;
-            playground[i][j].y = j;
-            playground[i][j].isWall = !(j == 3 && i==0 )&& (i == 0 || i == (n - 1) || (j == 0 ) || j == (m - 1));
-            if (playground[i][j].isWall && !(i == 3 && j == 0)) { playground[i][j].type = stone; }
-            else { playground[i][j].type = wood; }
-        }
-    }
-
+    std::vector<std::vector<int>> numbers;
     std::string line;
-    while (std::getline(std::cin, line))
-    {
+    std::map<int, std::vector<int>> histogram;
+    std::vector<int> result;
+    int line_index = 1;
 
-        std::vector<char> v(line.begin(), line.end());
+    int l;
+    int n;
+    std::cin>>l;
+    std::cin>>n;
 
-        for(char & it : v) {
-            print_level();
+    while (std::getline(std::cin, line)) {
 
-            struct update{int x;int y;};
-
-            update changes{x, y};
-
-            switch(it){
-                case 'l':
-                    changes.y = changes.y - 1;
-                    break;
-                case 'r':
-                    changes.y = changes.y + 1;
-                    break;
-                case 'u':
-                    changes.x = changes.x - 1;
-                    break;
-                case 'd':
-                    changes.x = changes.x + 1;
-                    break;
-            }
-
-            if(!playground[changes.x][changes.y].isWall) {
-                x = changes.x;
-                y = changes.y;
-            }
-
-            if (it == 'q')
-            {
-                return 0;
-            }
+        std::istringstream iss(line);
+        for (std::string s; iss >> s;){
+            result.push_back(stoi(s));
+            line_index++;
         }
+        if(line_index > n)
+            break;
 
     }
 
+    auto m = std::max_element(result.begin() , result.end()).operator[](0);
 
+    int k = ceil((double) m / (double) l);
 
+    for (int i = 0; i <= m; i += k) {
 
+        if(histogram.find(i) == histogram.end()) {
+            histogram[i] = std::vector<int>();
+        }
+        for (std::vector<int>::const_iterator v = result.begin(); v != result.end(); ++v) {
 
-
+            if( *v >= i && * v < i + k) {
+                histogram.at(i).push_back(*v);
+            }
+        }
+        std::cout << std::to_string(i) + ": " + std::to_string(histogram.at(i).size()) << std::endl;
+    }
 }
-
