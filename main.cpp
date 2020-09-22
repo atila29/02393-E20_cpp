edc96afedcecff4ff159c01d2fc2fcef91f9cc6a
Mathias Petersen <mathias.petersen95@gmail.com>
2020-09-22 02:52:11 +0200

    Solve 3.1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 
 typedef enum {
@@ -11,8 +12,8 @@ typedef struct {
     material type;
 } field;
 
-#define n 16
-#define m 12
+#define n 12
+#define m 16
 
 
 field playground[n][m];
@@ -24,7 +25,7 @@ void print_level() {
         for (int j = 0; j < m; j++) {
 
             if (playground[i][j].x == x && playground[i][j].y == y) {
-                std::cout<<"0";
+                std::cout<<"O";
             }
             else if (playground[i][j].isWall) {
                 std::cout<<"*";
@@ -42,7 +43,7 @@ int main() {
         for (int j = 0; j < m; j++) {
             playground[i][j].x = i;
             playground[i][j].y = j;
-            playground[i][j].isWall = (i == 0 || i == (n - 1) || (j == 0 && i != 3) || j == (m - 1));
+            playground[i][j].isWall = !(j == 3 && i==0 )&& (i == 0 || i == (n - 1) || (j == 0 ) || j == (m - 1));
             if (playground[i][j].isWall && !(i == 3 && j == 0)) { playground[i][j].type = stone; }
             else { playground[i][j].type = wood; }
         }
@@ -52,30 +53,41 @@ int main() {
     while (std::getline(std::cin, line))
     {
 
-        if (line == "l")
-        {
-            y--;
-        }
-        else if (line == "r")
-        {
-            y++;
-        }
-        else if (line == "u")
-        {
-            x--;
-        }
-        else if (line == "d")
-        {
-            x++;
+        std::vector<char> v(line.begin(), line.end());
+
+        for(char & it : v) {
+            print_level();
+
+            struct update{int x;int y;};
+
+            update changes{x, y};
+
+            switch(it){
+                case 'l':
+                    changes.y = changes.y - 1;
+                    break;
+                case 'r':
+                    changes.y = changes.y + 1;
+                    break;
+                case 'u':
+                    changes.x = changes.x - 1;
+                    break;
+                case 'd':
+                    changes.x = changes.x + 1;
+                    break;
+            }
+
+            if(!playground[changes.x][changes.y].isWall) {
+                x = changes.x;
+                y = changes.y;
+            }
+
+            if (it == 'q')
+            {
+                return 0;
+            }
         }
 
-
-        print_level();
-
-        if (line == "q")
-        {
-            break;
-        }
     }
 
 
