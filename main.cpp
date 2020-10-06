33a2980cb932f59cccacd6740a6be7e7595fa671
Mathias Petersen <mathias.petersen95@gmail.com>
2020-10-06 14:26:16 +0200

    Solve 5.1

--- main.cpp
+++ main.cpp
@@ -10,42 +10,34 @@
 
 int main() {
     std::string line;
-    std::string command;
-    std::multiset<double> bag;
+    std::map<std::string, std::multiset<int>> sets;
 
-    while (std::getline(std::cin, line)) {
+    std::getline(std::cin, line);
 
-        std::istringstream iss(line);
-        for (std::string s; iss >> s;){
-            if(s == "add" || s == "del" || s == "qry") {
-                command = s;
+    std::string set_name;
+    std::istringstream iss(line);
+    for (std::string s; iss >> s;) {
+        if(!set_name.empty()) {
+            if ( sets.find(set_name) == sets.end() ) {
+                // not found
+                std::multiset<int> new_set;
+                new_set.insert(stoi(s));
+                sets[set_name] = new_set;
+            } else {
+                sets[set_name].insert(stoi(s));
             }
-            else if(s == "quit") {
-                return 0;
-            }
-            else {
-                double n = std::stod(s);
-                if(command == "add") {
-                    bag.insert(n);
-                }
-                else if(command == "del") {
-
-                    auto itr = bag.find(n);
-                    if(itr!=bag.end()){
-                        bag.erase(itr);
-                    }
-                }
-                else if(command == "qry") {
-
-                    auto itr = bag.find(n);
-                    if(itr!=bag.end()){
-                        std::cout<<"T";
-                    }
-                    else {
-                        std::cout<<"F";
-                    }
-                }
-            }
+            set_name = "";
         }
+        else {
+            set_name = s;
+        }
+    }
+
+    for (const auto& p : sets) {
+
+        std::stringstream  s;
+        copy(p.second.begin(),p.second.end(), std::ostream_iterator<int>(s," "));
+        std::cout<<s.str();
+
     }
 }
