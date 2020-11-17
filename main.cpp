99ea8cabb197816e6f104ca065e9aa695cc15122
Mathias Petersen <mathias.petersen95@gmail.com>
2020-11-17 14:37:19 +0100

    Solve 9.2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <string>
 #include <iostream>
 #include <sstream>
-
-long double fib(int n);
+#include <algorithm>
 
 using namespace std;
+
+long d(string u, string v);
+
+
+long f(char e, char e1);
+
+string replace(string s);
 
 int main(){
 
@@ -16,23 +22,43 @@ int main(){
     const char delim = ' ';
 
 
-    string tmp;
     stringstream ss(line);
 
-
-    while (getline(ss, tmp, delim)) {
-        cout << fib(stoi(tmp)) << " ";
-
-    }
+    string word1;
+    getline(ss, word1, delim);
 
 
+    string word2;
+    getline(ss, word2, delim);
+
+    cout << d(word1, word2);
 
     
     return 0;
 }
 
-long double fib(int n) {
-    if (n <= 1)
-        return 1;
-    return fib(n-1) + fib(n-2);
+long d(const string u, const string v) {
+
+    if(u.length() == 0)
+        return v.length();
+    if(v.length() == 0)
+        return u.length();
+
+
+    long a = d(replace(u), v) + 1;
+    long b = d(u, replace(v)) + 1;
+    long c = d(replace(u), replace(v)) + f(u[0], v[0]);
+
+    return min({a,b,c});
 }
+
+string replace(const string s) {
+    string tmp = s;
+    return tmp.replace(0,1,"");
+}
+
+
+long f(char e, char e1) {
+    return e == e1 ? 0 : 1;
+}
+
